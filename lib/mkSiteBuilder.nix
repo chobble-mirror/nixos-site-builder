2106a3a41b88a82c4c2b69757c7623033913d620
Stefan <git@stefn.co.uk>
2024-12-03 02:07:01 +0000

    Remove redundant cores flag

--- lib/mkSiteBuilder.nix
+++ lib/mkSiteBuilder.nix
@@ -95,8 +95,6 @@ pkgs.writeShellApplication {
           --no-link \ # don't pollute /nix/store
           --option trusted-public-keys "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" \
           --option substituters "https://nix-community.cachix.org https://cache.nixos.org" \
-          --cores 0 \
-          --max-jobs auto \
         ) || fail "nix-build failed"
       else
         echo "No default.nix found, copying files directly..."

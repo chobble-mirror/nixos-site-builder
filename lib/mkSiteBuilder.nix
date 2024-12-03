2cc605ca0a6863731def89f99493cf41a987a390
Stefan <git@stefn.co.uk>
2024-12-03 02:10:17 +0000

    Fix errant \

--- lib/mkSiteBuilder.nix
+++ lib/mkSiteBuilder.nix
@@ -94,8 +94,7 @@ pkgs.writeShellApplication {
         source_dir=$(nix-build \
           --no-link \ # don't pollute /nix/store
           --option trusted-public-keys "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" \
-          --option substituters "https://nix-community.cachix.org https://cache.nixos.org" \
-        ) || fail "nix-build failed"
+          --option substituters "https://nix-community.cachix.org https://cache.nixos.org") || fail "nix-build failed"
       else
         echo "No default.nix found, copying files directly..."
         source_dir="."

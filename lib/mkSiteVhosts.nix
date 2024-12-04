24395f05f8d0e8cc92cde3f2b5054c665be41a48
Stefan <git@stefn.co.uk>
2024-12-04 21:44:54 +0000

    Share logging setup with the www- redirect

--- lib/mkSiteVhosts.nix
+++ lib/mkSiteVhosts.nix
@@ -5,6 +5,21 @@ let
   mkVhost = domain: cfg:
     let
       host = cfg.host or "caddy";
+      loggingConfig = domain: ''
+        log {
+          output file /var/log/caddy/access-${domain}.log {
+            roll_size 100mb
+            roll_keep 1
+            roll_keep_for 24h
+          }
+          format filter {
+            request>headers>User-Agent delete
+            request>headers>Cookie delete
+            request>remote_ip ip_mask 16 32
+            request>client_ip ip_mask 16 32
+          }
+        }
+      '';
     in
     if host != "caddy" then {} else
     let
@@ -14,26 +29,13 @@ let
       listenAddresses = ["0.0.0.0"];
       extraConfig = ''
         root * /var/www/${domain}
+        ${loggingConfig domain}
         file_server
         encode gzip zstd
 
         handle_errors {
           rewrite * /{err.status_code}.html
           file_server
-        }
-
-        log {
-          output file /var/log/caddy/access-${domain}.log {
-            roll_size 100mb
-            roll_keep 1
-            roll_keep_for 24h
-          }
-          format filter {
-			      request>headers>User-Agent delete
-						request>headers>Cookie delete
-						request>remote_ip ip_mask 16 32
-						request>client_ip ip_mask 16 32
-		      }
         }
 
         @static {
@@ -46,6 +48,7 @@ let
     "${protocol}://www.${domain}" = {
       listenAddresses = ["0.0.0.0"];
       extraConfig = ''
+        ${loggingConfig domain}
         redir ${protocol}://${domain}{uri} permanent
       '';
     };

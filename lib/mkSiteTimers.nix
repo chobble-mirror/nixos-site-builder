5bea11a65af0208c5dbb8dec3b558d0449e64199
Stefan <git@stefn.co.uk>
2024-12-04 22:54:09 +0000

    Stagger rebuilds across the five minutes

--- lib/mkSiteTimers.nix
+++ lib/mkSiteTimers.nix
@@ -3,19 +3,42 @@
 sites:
 let
   inherit (utils) shortHash;
+
+  timerConfig = rec {
+    interval = rec {
+      minutes = 5;
+      seconds = minutes * 60;
+    };
+    initialDelay = "5min";
+  };
+
   mkTimer = domain: cfg:
     let
       serviceUser = "site-${shortHash domain}-builder";
+      hashString = builtins.hashString "sha256" domain;
+      hashNum = builtins.foldl'
+        (sum: c: sum + builtins.stringLength (toString c))
+        0
+        (builtins.split "" hashString);
+      offset = hashNum - (builtins.div
+        hashNum
+        timerConfig.interval.seconds *
+        timerConfig.interval.seconds);
+      minutes = builtins.toString (builtins.div offset 60 + 100);
+      seconds = builtins.toString (offset - (builtins.div offset 60 * 60) + 100);
+      paddedMinutes = builtins.substring 1 2 minutes;
+      paddedSeconds = builtins.substring 1 2 seconds;
     in {
       "${serviceUser}" = {
         wantedBy = [ "timers.target" ];
         timerConfig = {
-          OnBootSec = "5min";
-          OnUnitActiveSec = "5min";
+          OnBootSec = timerConfig.initialDelay;
+          OnUnitActiveSec = "*:${paddedMinutes}:${paddedSeconds}";
         };
       };
     };
 in
-builtins.foldl' (acc: domain:
-  acc // (mkTimer domain sites.${domain})
-) {} (builtins.attrNames sites)
+builtins.foldl'
+  (acc: domain: acc // (mkTimer domain sites.${domain}))
+  {}
+  (builtins.attrNames sites)

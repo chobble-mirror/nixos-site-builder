bb7763e2564e9aecfa47fe67111b27d82c264329
Stefan <git@stefn.co.uk>
2024-12-10 04:53:33 +0000

    Trimmed inputs

--- flake.nix
+++ flake.nix
@@ -3,48 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    jekyll-builder.url =
-      "git+https://git.chobble.com/chobble/nix-jekyll-builder";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, jekyll-builder }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
+    in
+    {
       nixosModules.default = import ./modules/site-builder.nix;
 
-      checks = forAllSystems (system:
+      checks = forAllSystems (
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
           utils = import ./lib/utils.nix;
-        in import ./tests { inherit pkgs utils; });
+        in
+        import ./tests { inherit pkgs utils; }
+      );
 
       # Convenience apps for running specific test suites
       apps = forAllSystems (system: {
         test-unit = {
           type = "app";
-          program = (import ./tests/unit {
-            pkgs = nixpkgs.legacyPackages.${system};
-            lib = nixpkgs.lib;
-          }).program;
+          program =
+            (import ./tests/unit {
+              pkgs = nixpkgs.legacyPackages.${system};
+              lib = nixpkgs.lib;
+            }).program;
         };
         test-integration = {
           type = "app";
-          program = (import ./tests/integration {
-            pkgs = nixpkgs.legacyPackages.${system};
-            lib = nixpkgs.lib;
-          }).program;
+          program =
+            (import ./tests/integration {
+              pkgs = nixpkgs.legacyPackages.${system};
+              lib = nixpkgs.lib;
+            }).program;
         };
       });
 
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ nixpkgs-fmt shellcheck ];
+            buildInputs = with pkgs; [
+              nixpkgs-fmt
+              shellcheck
+            ];
           };
-        });
+        }
+      );
     };
 }
